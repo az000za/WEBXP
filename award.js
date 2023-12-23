6a54be810f9006057e988cdadffbe05bb2cbb658
AZ000ZA <150498639+az000za@users.noreply.github.com>
2023-12-23 13:52:51 -0500

    Update award.js

--- award.js
+++ award.js
@@ -1,1 +1,25 @@
+class Award {
+  constructor(name, category, description, dateReceived, issuer) {
+    this.name = name;
+    this.category = category;
+    this.description = description;
+    this.dateReceived = dateReceived;
+    this.issuer = issuer;
+    // this.cryptoSigned = // blockchain id stuff goes here
+  }
 
+  displayInfo() {
+    console.log(`Award Name: ${this.name}`);
+    console.log(`Category: ${this.category}`);
+    console.log(`Description: ${this.description}`);
+    console.log(`Date Received: ${this.dateReceived}`);
+  }
+}
+
+// // Creating an instance of the Award class
+// const sampleAward = new Award(
+//   "Outstanding Contribution Award",
+//   "Professional Recognition",
+//   "Awarded for exceptional contributions to the company.",
+//   "December 15, 2023"
+// );
